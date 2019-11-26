0fb227911e012d800bf9f58f613b7fc5d8853315
Your Name <you@example.com>
2019-11-26 13:24:43 +0900

    Improve "kurai" accuracy

--- main.ts
+++ main.ts
@@ -22,13 +22,76 @@ namespace gp2 {
     export function turnOFF(): void {
         pins.digitalWritePin(DigitalPin.P1, 0)
     }
-    //% blockId=is_dark block="暗い"
+
+
+    let _今まで暗い: boolean = false;
+    const _暗い判定閾値: number = 8;
+    const _明るい判定閾値: number = 15;
+    const _HYSTERESIS: number = _明るい判定閾値 - _暗い判定閾値;
+
+
+    /**
+     * micro:bit本体の明るさセンサーが暗い場合（8未満）に真を返します。
+     */
+    //% blockId=is_dark block="is dark"
+    //% group="micro:bit本体"
     export function isDark(): boolean {
-        if (input.lightLevel() < 30)
-            return true;
-        else
-            return false;
+        return _isDark(_暗い判定閾値, _明るい判定閾値);
+
     }
+
+    /* 明るさの平均を取る */
+    function _lightLevelSampling(): number {
+        const CYCLE_SAMPLE_NUM: number = 50
+        let accum明るさ: number = 0;
+        for (let i = 0; i < CYCLE_SAMPLE_NUM; i++) {
+            accum明るさ += input.lightLevel();
+            basic.pause(1);
+        }
+        let 明るさ = accum明るさ / CYCLE_SAMPLE_NUM;
+        return 明るさ;
+    }
+
+    /* 暗い判定本体 */
+    function _isDark(暗い判定閾値: number, 明るい判定閾値: number): boolean {
+        if ((暗い判定閾値 > 明るい判定閾値)
+            || (暗い判定閾値 < 0)
+            || (暗い判定閾値 > 255)
+            || (明るい判定閾値 < 0)
+            || (明るい判定閾値 > 255)) {
+            control.assert(false, "threshold is abnormal");
+        }
+
+        let 現在の明るさ = _lightLevelSampling();
+
+        const 暗い: boolean = true;
+        const 明るい: boolean = false;
+
+        if (_今まで暗い) { //現在まで暗い環境だったとき。明るいかを判定
+            if (現在の明るさ > 明るい判定閾値) {
+                _今まで暗い = 明るい;
+                return 明るい; //現在は明るい
+            }
+            else {
+                _今まで暗い = 暗い;
+                return 暗い; //現在は暗い
+            }
+        }
+        else { // 現在まで明るい環境だったとき。暗いかを判定
+            if (現在の明るさ < 暗い判定閾値) {
+                _今まで暗い = 暗い;
+                return 暗い; //現在は暗い
+            }
+            else {
+                _今まで暗い = 明るい;
+                return 明るい; //現在は明るい
+            }
+        }
+        control.assert(false);
+    }
+
+
+
     function getAnalogValue(p: AnalogPin): number {
         let arr: number[] = [];
         // Median filter
@@ -51,15 +114,36 @@ namespace gp2 {
             1023
         )
     }
-    //% blockId=brightness_determination block="%v より %flag"
-    //% v.min=0 v.max=255
-    export function brightnessDetermination(v: number, flag: BrightnessEvent): boolean {
-        let res: boolean = true;
-        if (flag == 2)
-            res = !res;
-        if (input.lightLevel() < v)
-            return res;
-        else
-            return !res;
+    
+    /**
+     * micro:bit本体の明るさセンサーが閾値より暗い（または明るい）場合に真を返します。
+     * @param lightThreshold number of brightness-threshold, eg: 15
+     */
+    //% blockId=brightness_determination
+    //% block="Than %lightThreshold, %settingDarkOrBright"
+    //% lightThreshold.min=0 lightThreshold.max=255
+    //% group="micro:bit本体"
+    export function brightnessDetermination(lightThreshold: number, settingDarkOrBright: DarkOrBrightSpecified): boolean {
+        if (_HYSTERESIS < 0) { control.assert(false); }
+        if (lightThreshold < 0) {
+            lightThreshold = 0;
+        }
+        if (lightThreshold > 255) {
+            lightThreshold = 255;
+        }
+
+        if (settingDarkOrBright === DarkOrBrightSpecified.IS_DARK) {
+            let 暗い判定閾値: number = lightThreshold;
+            let 明るい判定閾値: number = lightThreshold + _HYSTERESIS;
+            if (明るい判定閾値 > 255) { 明るい判定閾値 = 255; }
+            return _isDark(暗い判定閾値, 明るい判定閾値);
+        }
+        else if (settingDarkOrBright === DarkOrBrightSpecified.IS_BRIGHT) {
+            let 暗い判定閾値: number = lightThreshold - _HYSTERESIS;
+            let 明るい判定閾値: number = lightThreshold;
+            if (暗い判定閾値 < 0) { 暗い判定閾値 = 0; }
+            return !_isDark(暗い判定閾値, 明るい判定閾値);
+        }
+        control.assert(false); return false;
     }
 }
